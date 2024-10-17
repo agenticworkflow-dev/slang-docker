b24f4c86abdd92e4428696f02da17e1c0096de97
agenticworkflow <agenticworkflow@gmail.com>
2024-10-17 22:47:03 +0800

    change curl test to chat

--- tests/test_curl.sh
+++ tests/test_curl.sh
@@ -1,7 +1,7 @@
 curl http://localhost:30000/generate \
   -H "Content-Type: application/json" \
   -d '{
-    "text": "what is the highest peak in the world?",
+    "text": "<|im_start|>what is the highest peak in the world?<|im_end|>",
     "sampling_params": {
       "max_new_tokens": 256,
       "temperature": 0
